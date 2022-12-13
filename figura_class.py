83ff02038b06ebe829ca1550c14ae1aea63bd028
Jorge Blazquez <jblazquez2701@gmail.com>
2022-12-13 20:49:02 +0100

    raquetas,bola y movimiento

--- figura_class.py
+++ figura_class.py
@@ -1,7 +1,7 @@
 import pygame as pg
 
 class Pelota:
-    def __init__(self,pos_x,pos_y,radio=10,color=(255,255,255),vx=1,vy=1):
+    def __init__(self,pos_x,pos_y,radio=15,color=(255,255,255),vx=1,vy=1):
         self.pos_x = pos_x
         self.pos_y = pos_y
         self.radio = radio
@@ -14,14 +14,14 @@ class Pelota:
 
 
 class Raqueta:
-     def __init__(self,pos_x,pos_y,w=5,h=50,color=(255,255,255),vx=1,vy=1):
+     def __init__(self,pos_x,pos_y,w=30,h=100,color=(255,255,255),vx=1,vy=1):
         self.pos_x = pos_x
         self.pos_y = pos_y
-        self.pos_w = w
-        self.pos_h = h
+        self.w = w
+        self.h = h
         self.color = color
         self.vx = vx
         self.vy = vy
 
      def dibujar(self,pantalla):
-        pg.draw.rect(pantalla,self.color,(self.pos_x,self.pos_y),self.w,self.h)   
+        pg.draw.rect(pantalla,self.color,(self.pos_x-(self.w//2),self.pos_y-(self.h//2),self.w,self.h))   
