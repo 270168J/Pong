2363b39652d192fbc39d16926aea8e28aa9d45e6
Jorge Blazquez <jblazquez2701@gmail.com>
2022-12-15 20:55:16 +0100

    mas cambios

--- figura_class.py
+++ figura_class.py
@@ -12,9 +12,26 @@ class Pelota:
     def dibujar(self,pantalla):
         pg.draw.circle(pantalla,self.color,(self.pos_x,self.pos_y),self.radio)
 
+    def mover(self,x_max=800,y_max=600):
+        self.pos_x += self.vx
+        self.pos_y += self.vy
+        #cambiamos para que si se marca gol a un lado salga exactamante
+        #en el sentido contrario rebotando
+        
+        if self.pos_y >= y_max- self.radio or self.pos_y < self.radio:
+            self.vy = self.vy*-1
+
+        if self.pos_x >= x_max or self.pos_x<0:
+            self.pos_x = x_max//2
+            self.pos_y = y_max//2
+
+            self.vy *= -1
+            self.vx *= -1
+        
+
 
 class Raqueta:
-     def __init__(self,pos_x,pos_y,w=30,h=100,color=(255,255,255),vx=1,vy=1):
+     def __init__(self,pos_x,pos_y,w=20,h=100,color=(255,255,255),vx=1,vy=1):
         self.pos_x = pos_x
         self.pos_y = pos_y
         self.w = w
@@ -24,4 +41,13 @@ class Raqueta:
         self.vy = vy
 
      def dibujar(self,pantalla):
-        pg.draw.rect(pantalla,self.color,(self.pos_x-(self.w//2),self.pos_y-(self.h//2),self.w,self.h))   
+        pg.draw.rect(pantalla,self.color,(self.pos_x-(self.w//2),self.pos_y-(self.h//2),self.w,self.h))  
+
+
+     def mover(self,tecla_arriba,tecla_abajo,y_max=600,y_min=0):
+        estado_teclas = pg.key.get_pressed()#movimiento raquetas
+
+        if estado_teclas[tecla_arriba] == True and self.pos_y > (y_min+self.h//2):
+           self.pos_y -= 1
+        if estado_teclas[tecla_abajo] == True and self.pos_y < (y_max -self.h//2):
+           self.pos_y += 1
--- main.py
+++ main.py
@@ -1,22 +1,33 @@
 from figura_class import Pelota,Raqueta
 import pygame as pg
 
-pg.init()
+pg.init()#pantalla principal en bucle hata el game_over
 pantalla_principal = pg.display.set_mode((800,600))
 pg.display.set_caption("Pong")
 
 
+#definir la tasa de refresco de nuestro bucle de fotogramas fps=fotograma por segundo
+cronometro = pg.time.Clock()
+
 pelota = Pelota(400,300)
-raqueta1 = Raqueta(15,300)
-raqueta2 = Raqueta(782,300)
+raqueta1 = Raqueta(5,300)
+raqueta2 = Raqueta(795,300)
 
 game_over = False
 
 while not game_over:
+    #imprimir los milisegundos que tarda cada fotograma actualmente
+    vt = cronometro.tick(500)#vt=variable para controlar la velocidad entre fotogramas, puedes llamarlo como quieras
+    #print(vt)
     for evento in pg.event.get():
          if evento.type == pg.QUIT:
              game_over = True 
 
+
+    raqueta1.mover(pg.K_w,pg.K_s)
+    raqueta2.mover(pg.K_UP,pg.K_DOWN)
+
+    pelota.mover()
     """
     #mover raqueta
     if evento.type == pg.KEYDOWN:
@@ -27,22 +38,23 @@ while not game_over:
             print("ABAJO")
             raqueta1.pos_y += 3
     """
-    estado_teclas = pg.key.get_pressed()
+    """
+    estado_teclas = pg.key.get_pressed()#movimiento raquetas
 
     if estado_teclas[pg.K_UP] == True:
-        raqueta1.pos_y -= 3
+        raqueta1.pos_y -= 1
     if estado_teclas[pg.K_DOWN] == True:
-        raqueta1.pos_y += 3 
+        raqueta1.pos_y += 1 
 
     if estado_teclas[pg.K_UP] == True:
-        raqueta2.pos_y -= 3
+        raqueta2.pos_y -= 1
     if estado_teclas[pg.K_DOWN] == True:
-        raqueta2.pos_y += 3 
-    
+        raqueta2.pos_y += 1 
+    """
 
 
-    pantalla_principal.fill((0,128,24))
-    pg.draw.line(pantalla_principal, (255,255,255), (400,0), (400,600), width=2)
+    pantalla_principal.fill((0,128,24))#pintado de pantalla
+    pg.draw.line(pantalla_principal, (255,255,255), (400,0), (400,600), width=2)#linea del medio
 
 
     
